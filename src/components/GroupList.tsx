999ae5a92f0ff69ec62c25b5c6e5c01b84d1466e
hong.yuanlei <hong.yuanlei@rea-group.com>
2021-11-24 20:20:40 +0800

    add mui ui

---
+++ src/components/GroupList.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+
+const GroupList = () => {
+  return (
+    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+      <ListItemButton>
+        <ListItemText primary="Photos" secondary="Jan 9, 2014" />
+      </ListItemButton>
+      <ListItemButton>
+        <ListItemText primary="Work" secondary="Jan 7, 2014" />
+      </ListItemButton>
+      <ListItemButton>
+        <ListItemText primary="Vacation" secondary="July 20, 2014" />
+      </ListItemButton>
+    </List>
+  );
+};
+
+export default GroupList;
